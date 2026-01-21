7bb2d51046e15bae54b6f05dd1a98c3630e6394a
ahmed mbarek <mbarekahmed0000@outlook.com>
2026-01-21 08:28:49 +0100

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log(count)
+  const ref = useRef();
   return (
     <div>
-      <input type="text" onChange={(event)=> setCount(event.target.value)}/>
-      {count}
+      <input ref={ref} type="text" />
+      <button onClick={() => console.log(ref.current.value)}>print</button>
+      {/* doesnt work ! remove it ! */}
+      {ref.current.value}
+      {/* we use useRef for non rerendering things
+            When it changes → no re-render.
+          and we use useState for rerendering things
+            When the state changes → React re-renders → UI may change.
+          Does this value participate in rendering? -----> useState
+          otherwise ------> useRef
+      */}
     </div>
   );
 }
