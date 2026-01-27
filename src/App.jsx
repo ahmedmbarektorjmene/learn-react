76ebbd9538fab48c22550abde5ae973643674265
ahmed mbarek <mbarekahmed0000@outlook.com>
2026-01-27 18:54:18 +0100

    exercise for house on useEffect and cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const ref = useRef();
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  useEffect(() => {
+    const T = setInterval(() => {
+      console.log("hii");
+    }, 2000);
+
+    return () => {
+      clearInterval(T);
+    };
+  });
+
   return (
     <div>
-      <input ref={ref} type="text" />
-      <button onClick={() => console.log(ref.current.value)}>print</button>
-      {/* doesnt work ! remove it ! */}
-      {ref.current.value}
-      {/* we use useRef for non rerendering things
-            When it changes → no re-render.
-          and we use useState for rerendering things
-            When the state changes → React re-renders → UI may change.
-          Does this value participate in rendering? -----> useState
-          otherwise ------> useRef
-      */}
+      <button onClick={() => setCount1((prev) => prev + 1)}>{count1}</button>
+      <button onClick={() => setCount2((prev) => prev + 1)}>{count2}</button>
     </div>
   );
 }
-
 export default App;
